d269f0519985138e04495d0c70a2e810e52cca83
Like <like@ekil.sh>
2023-04-27 16:59:03 +0800

    .

---
+++ plugins/im-select.lua
@@ -0,0 +1,9 @@
+return {
+  "keaising/im-select.nvim",
+  lazy = false,
+  opts = {
+    default_im_select = "com.apple.keylayout.US",
+    disable_auto_restore = 0,
+    default_command = "im-select",
+  },
+}
